c3b8a808b683de5685229a1b30dc9c2426c65b9e
Tetiana Bortnyk <tatyanabortnik@gmail.com>
2023-02-19 17:34:13 +0100

    update

--- react_dashboard/src/hooks/useColor.jsx
+++ react_dashboard/src/hooks/useColor.jsx
@@ -3,9 +3,7 @@ import { useEffect, useState } from "react";
 import { TEXT_COLOR } from "../constants/theme";
 
 export default function useColor() {
-   const [color, setColor] = useState(
-      localStorage.getItem("color") ? localStorage.getItem("color") : TEXT_COLOR
-   );
+   const [color, setColor] = useState(localStorage.getItem("color"));
 
    useEffect(() => {
       localStorage.setItem("color", color);
--- react_dashboard/src/hooks/useTheme.jsx
+++ react_dashboard/src/hooks/useTheme.jsx
@@ -3,11 +3,7 @@ import { useState, useEffect } from "react";
 import { THEME_LIGHT, THEME_STORAGE_KEY } from "../constants/theme";
 
 export default function useTheme() {
-   const [theme, setTheme] = useState(
-      localStorage.getItem(THEME_STORAGE_KEY)
-         ? localStorage.getItem(THEME_STORAGE_KEY)
-         : THEME_LIGHT
-   );
+   const [theme, setTheme] = useState(localStorage.getItem(THEME_STORAGE_KEY));
 
    useEffect(() => {
       localStorage.setItem(THEME_STORAGE_KEY, theme);
